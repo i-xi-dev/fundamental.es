dd787f87c7832a4bc2f4ecbc9b26eb1cad72f318
i-xi-dev <naoyuki.isono@gmail.com>
2022-01-12 23:46:43 +0900

    1.5.4

--- src/stream_utils.test.ts
+++ src/stream_utils.test.ts
@@ -32,4 +32,76 @@ describe("StreamUtils.streamToAsyncGenerator", () => {
 
   });
 
+  it("streamToAsyncGenerator(ReadableStreamDefaultReader)/return", async () => {
+    let ti: NodeJS.Timeout;
+    const s = new ReadableStream({
+      start(controller: ReadableStreamDefaultController) {
+        let c = 0;
+        ti = setInterval(() => {
+          if (c >= 10) {
+            clearInterval(ti);
+            controller.close();
+            return;
+          }
+          c = c + 1;
+
+          let x = c.toString(10).padStart(2, "0");
+          controller.enqueue(x);
+        }, 10);
+      },
+    });
+
+    const chunks = StreamUtils.streamToAsyncGenerator(s.getReader());
+    const result = [];
+    let i = 0;
+    for await (const chunk of chunks) {
+      i = i + 1;
+      if (i > 5) {
+        break; //chunks.return();が呼ばれる
+      }
+      result.push(chunk);
+    }
+
+    assert.strictEqual(result.join(""), "0102030405");
+
+  });
+
+  it("streamToAsyncGenerator(ReadableStreamDefaultReader)/throw", async () => {
+    let ti: NodeJS.Timeout;
+    const s = new ReadableStream({
+      start(controller: ReadableStreamDefaultController) {
+        let c = 0;
+        ti = setInterval(() => {
+          if (c >= 10) {
+            clearInterval(ti);
+            controller.close();
+            return;
+          }
+          c = c + 1;
+
+          let x = c.toString(10).padStart(2, "0");
+          controller.enqueue(x);
+        }, 10);
+      },
+    });
+
+    const chunks = StreamUtils.streamToAsyncGenerator(s.getReader());
+    const result = [];
+    let i = 0;
+    try {
+      for (let i = 0; i < 10; i++) {
+        if (i >= 5) {
+          await chunks.throw("");
+        }
+        result.push((await chunks.next()).value);
+      }
+    }
+    catch (e) {
+      console.log(e);
+    }
+
+    assert.strictEqual(result.join(""), "0102030405");
+
+  });
+
 });
